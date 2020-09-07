b090b6fed8bc9cb9a6b2d979ba9ff194581198f6
Stanislav <semioys@gmail.com>
2020-09-07 14:24:56 +0300

    Logic for simple background color change was added

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', function() {
+  const randomColor = getRandomNumberFromArrayLenght(colors);
+  document.body.style.backgroundColor = colors[randomColor];
+  color.textContent = colors[randomColor];
+});
+
+function getRandomNumberFromArrayLenght(arr) {
+  return Math.floor((Math.random() * arr.length));
+}
